f3f1bcca71ca774c5bb2860919cf53b9b224e713
Astrid Varga <astrid.varga@gmx.de>
2018-11-12 14:44:12 +0100

    refactor x and y positioning to PVector location and velocity

--- out/source/bouncingBall_PVector.java
+++ out/source/bouncingBall_PVector.java
@@ -1,10 +1,41 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class bouncingBall_PVector extends PApplet {
+
+Ball b;
+
+public void setup() {
+  
+  b = new Ball();
+}
+
+public void draw() { 
+  
+  // Draw stuff
+  background(79,98,114);
+  b.displayBall();
+  b.moveBall();
+  b.checkBorders();
+}
 class Ball {
   
   // --------------------------------
   // Position and Size of Ball
   // --------------------------------
 
-  float ballSize = 50.0;   // Diameter of Ball
+  float ballSize = 50.0f;   // Diameter of Ball
 
   // float ballX, ballY;  // Position of Ball
   // => x and y can be expressed in PVector object (location)
@@ -20,8 +51,8 @@ class Ball {
   PVector velocity;
 
   // Color
-  color ballColor;
-  color strokeColor;
+  int ballColor;
+  int strokeColor;
 
   // --------------------------------
   // Constructor
@@ -40,7 +71,7 @@ class Ball {
     // ballX = random(width);
     // ballY = random(height);
     location = new PVector(random(width), random(height));
-    velocity = new PVector(3.0, 2.3);
+    velocity = new PVector(3.0f, 2.3f);
 
     ellipseMode(CENTER);
   
@@ -50,7 +81,7 @@ class Ball {
   // Display Ball
   // --------------------------------
 
-    void displayBall(){
+    public void displayBall(){
       stroke(strokeColor);
       fill(ballColor);
       ellipse(location.x, location.y, ballSize, ballSize);
@@ -60,7 +91,7 @@ class Ball {
   // Move circle by adding to it's coordinates
   // -------------------------------------------
 
-  void moveBall() {
+  public void moveBall() {
     // ballX = ballX + xSpeed;
     // ballY = ballY + ySpeed;
     // => translates into the following PVectors
@@ -72,7 +103,7 @@ class Ball {
   // Check if Ball touches borders, if yes change direction
   // ---------------------------------------------------------
 
-  void checkBorders() {
+  public void checkBorders() {
     // (https://www.youtube.com/watch?v=YIKRXl3wH8Y&index=5&list=PLRqwX-V7Uu6YqykuLs00261JCqnL_NNZ_)
     
     // if (ballX > width || ballX < 0 ) {
@@ -85,6 +116,16 @@ class Ball {
       velocity.x = velocity.x * -1; 
     } else if (location.y > height || location.y < 0) {
       velocity.y = velocity.y * -1;
+    }
+  }
+}
+  public void settings() {  size(650, 750); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "bouncingBall_PVector" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
     }
   }
 }
