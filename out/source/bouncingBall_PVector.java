e2799fa5e7f063937e57d384e166208c7f74c2ad
Astrid Varga <astrid.varga@gmx.de>
2018-11-28 13:49:21 +0100

    Change Ball Color

--- out/source/bouncingBall_PVector.java
+++ out/source/bouncingBall_PVector.java
@@ -145,7 +145,7 @@ class Ball {
     // ballColor = color(173,252,249);
 
     // random color
-    ballColor = color(random(50), random(255), random(0));
+    ballColor = color(random(50), random(255), random(200));
     strokeColor = ballColor;
     
   
